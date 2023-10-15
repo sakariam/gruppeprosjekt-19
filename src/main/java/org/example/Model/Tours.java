0d1a838a76869f3ca8e3f310f5c3578ee9119a11
BrothaZylo <83866259+BrothaZylo@users.noreply.github.com>
2023-10-15 20:20:03 +0200

    Added methods:
    isAvailable()
    userBookTour(User user)
    userCancelTour(User user)
    getAvailableSpots()
    getTourDetails()
    getTourRevenue()
    getters and setters

--- src/main/java/org/example/Model/Tours.java
+++ src/main/java/org/example/Model/Tours.java
@@ -1,26 +1,109 @@
 package org.example.Model;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Tours {
-    private int id;
-    private Date available_dates;
-    private String title;
-    private String location;
+    private int tourId;
+    private String tourName;
     private String description;
-    private double rating;
+    private double price;
+    private Guide guide;
+    private int capacity;
+    private List<User> bookedUsers;
 
-
-    // TODO: 26.09.2023 Lag constructer, getters og setters etter alle variablene
-
-    //Check if date is available
-    public void isAvailable(){
-
+    //Checking if there are any spots left in the tour
+    public boolean isAvailable(){
+        return bookedUsers.size() < capacity;
     }
-    public void addReview(){
-
+    //Adding user to the tour
+    public boolean userBookTour(User user){
+        if (bookedUsers.size() < capacity){
+            bookedUsers.add(user);
+            return true;
+        }
+        else {
+            return false;
+        }
     }
-    public void addBooking(){
-
+    //removing user from tour
+    public boolean userCancelTour(User user) {
+        if (bookedUsers.contains(user)) {
+            bookedUsers.remove(user);
+            return true;
+        } else {
+            return false;
+        }
     }
+    //Checks for how many spots left in the tour
+    public int getAvailableSpots(){
+        return capacity - bookedUsers.size();
+    }
+    //Returns Tour details
+    public String getTourDetails(){
+        return "Tour title:" + tourName + "\nDescription: " + description + "\nPrice: " + price + "kr"; //+ guide.getName() Add this after we add getName in Guide class
+    }
+    //Returns how much the guide will get from the tour
+    public double getTourRevenue(){
+        return bookedUsers.size() * price;
+    }
+
+    //Getters and setter
+    public int getTourId() {
+        return tourId;
+    }
+
+    public void setTourId(int tourId) {
+        this.tourId = tourId;
+    }
+
+    public String getTourName() {
+        return tourName;
+    }
+
+    public void setTourName(String tourName) {
+        this.tourName = tourName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Guide getGuide() {
+        return guide;
+    }
+
+    public void setGuide(Guide guide) {
+        this.guide = guide;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public List<User> getBookedUsers() {
+        return bookedUsers;
+    }
+
+    public void setBookedUsers(List<User> bookedUsers) {
+        this.bookedUsers = bookedUsers;
+    }
+
 }
