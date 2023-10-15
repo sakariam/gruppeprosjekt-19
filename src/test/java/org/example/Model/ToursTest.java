37e20a3d5cfb96551d1e75390c323570edc0a85c
BrothaZylo <83866259+BrothaZylo@users.noreply.github.com>
2023-10-15 20:49:40 +0200

    Added Junit dependency and test file Added test file and methods to test

---
+++ src/test/java/org/example/Model/ToursTest.java
@@ -0,0 +1,30 @@
+package org.example.Model;
+
+import org.junit.jupiter.api.Test;
+
+class ToursTest {
+    // TODO: 15.10.2023 Change methodn names so it describes what it is doing. And check both for true and false where you are able to 
+    @Test
+    void isAvailable() {
+    }
+
+    @Test
+    void userBookTour() {
+    }
+
+    @Test
+    void userCancelTour() {
+    }
+
+    @Test
+    void getAvailableSpots() {
+    }
+
+    @Test
+    void getTourDetails() {
+    }
+
+    @Test
+    void getTourRevenue() {
+    }
+}
