9acca3595b669cf6f864e1245576d2b6428572d4
BrothaZylo <83866259+BrothaZylo@users.noreply.github.com>
2023-10-17 22:58:32 +0200

    Added login for admin,user and guide. And connected Main with controller

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,16 @@
 package org.example;
 
+import org.example.Controller.Controller;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-// TODO: 26.09.2023 Her sender vi json filen til repository
 
+
+        // TODO: 17.10.2023 Under sender vi json filene til repository 
+
+        // TODO: 17.10.2023 Add the repositories inside the parenthesis in Controller();
+        Controller controller = new Controller();
+
+        controller.login();
+    }
 }
