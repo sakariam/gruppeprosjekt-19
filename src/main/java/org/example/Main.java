09fb2fba46320e935a28a6aefe27fd3c2b9f5a98
sakariam <111963152+sakariam@users.noreply.github.com>
2023-10-30 11:49:03 +0100

    torsdag av sakaria

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,11 +7,11 @@ public class Main {
     public static void main(String[] args) {
 
         // TODO: 17.10.2023 Under sender vi json filene til repository
-        GuideRepository guideRepository = new GuideJSONRepository("src/data/tour.json");
+        //GuideRepository guideRepository = new GuideJSONRepository();
         ToursRepository toursRepository = new ToursJSONRepository("src/data/tour.json");
         //UserRepository userRepository = new UserJSONRepository();
         // TODO: 17.10.2023 Add the repositories inside the parenthesis in Controller();
-        Controller controller = new Controller(toursRepository,guideRepository);
+        Controller controller = new Controller(toursRepository);
 
         controller.login();
     }
---
+++ src/main/java/org/example/Model/Users.java
@@ -0,0 +1,115 @@
+package org.example.Model;
+
+
+import java.util.List;
+import java.util.Scanner;
+
+
+// TODO: 26.09.2023 Lag constructer, getters og setters etter alle variablene: Done
+public class Users {
+    private String username;
+    private String first_name;
+    private String last_name;
+    private List<Tours> tours;
+
+
+    public Users( String username, String first_name, String last_name, List<Tours> favorite_tours) {
+        this.username = username;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.tours= tours;
+    }
+
+
+    public Users() {
+
+    }
+
+
+    public String makeProfile(){
+        String profile= "Username: " + username + "\n";
+        profile +="first-name: " + first_name + "\n";
+        profile += "last-name: " + last_name + "\n";
+
+        return profile;
+
+    }
+
+
+
+    public String purchaseBooking() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the guide's name: ");
+        String guideName = scanner.nextLine();
+
+        System.out.print("How many people?: ");
+        int numberOfPeople = scanner.nextInt();
+
+        System.out.print("The price: ");
+        double price = scanner.nextDouble();
+
+        System.out.print("Is the information correct? (yes/no): ");
+        String confirmation = scanner.next();
+        
+        scanner.close();
+
+        if (confirmation.equalsIgnoreCase("yes")) {
+            String booking = "Guide: " + guideName + "\n";
+            booking += "How many people?: " + numberOfPeople + "\n";
+            booking += "Price: " + price + "\n";
+
+
+            return booking;
+        }  else {
+            return "booking is not confirmed. try again";
+        }
+
+
+    }
+
+
+    public void deleteProfile(){
+
+    }
+    public void addFavoriteTour(){
+
+    }
+    public void removeFavoriteTour(){
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public List<Tours> getours() {
+        return tours;
+    }
+
+    public void setFavorite_tours(List<Tours> favorite_tours) {
+        this.tours = favorite_tours;
+    }
+}
+
+
