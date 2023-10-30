699f92b137eca1a58d386ec3650bbd83dc3a5042
hiofsam <43729313+hiofsam@users.noreply.github.com>
2023-10-30 10:27:47 +0100

    added test TourIsAvailable

--- src/test/java/org/example/Model/ToursTest.java
+++ src/test/java/org/example/Model/ToursTest.java
@@ -1,11 +1,18 @@
 package org.example.Model;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ToursTest {
     // TODO: 15.10.2023 Change methodn names so it describes what it is doing. And check both for true and false where you are able to 
     @Test
-    void isAvailable() {
+    void TourIsAvailable() {
+        // Arrange
+        Tours Tour = new Tours("Fredriksten", "Fredriksten festning", 200, 15);
+        // Act
+        boolean isAvailable = Tour.getCapacity()>0;
+        //Assert
+        assertTrue(isAvailable,"The tour is available");
     }
 
     @Test
