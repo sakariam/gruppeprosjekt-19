3ef1238cfb097203e498f41a5d2cce02cb8142f3
samhu-hue <55948747+samhu-hue@users.noreply.github.com>
2023-11-08 12:48:19 +0100

    Testet getters og setters i Tours

--- src/test/java/org/example/Model/ToursTest.java
+++ src/test/java/org/example/Model/ToursTest.java
@@ -1,5 +1,6 @@
 package org.example.Model;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -7,6 +8,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ToursTest {
     // TODO: 15.10.2023 Change methodn names so it describes what it is doing. And check both for true and false where you are able to 
+    private Tours tour;
+
+
+
     @Test
     void TourIsAvailable() {
         // Arrange
@@ -27,22 +32,23 @@ class ToursTest {
     }
 
     @Test
-    void userBookTour() {
+    public void testGettersAndSetters() {
+        tour = new Tours("OsloBy", "Guidet tur rundt Oslo", 500, 10);
+
+        assertEquals("OsloBy", tour.getTitle());
+        assertEquals("Guidet tur rundt Oslo", tour.getDescription());
+        assertEquals(500, tour.getPrice(), 0.001); // Sjekk double med en toleranse på 0.001
+        assertEquals(10, tour.getCapacity());
+
+        tour.setTitle("Operahuset");
+        tour.setDescription("Tour rundt operahuset");
+        tour.setPrice(200.0);
+        tour.setCapacity(20);
+
+        assertEquals("Operahuset", tour.getTitle());
+        assertEquals("Tour rundt operahuset", tour.getDescription());
+        assertEquals(200.0, tour.getPrice(), 0.001);
+        assertEquals(20, tour.getCapacity());
     }
 
-    @Test
-    void userCancelTour() {
-    }
-
-    @Test
-    void getAvailableSpots() {
-    }
-
-    @Test
-    void getTourDetails() {
-    }
-
-    @Test
-    void getTourRevenue() {
-    }
 }
